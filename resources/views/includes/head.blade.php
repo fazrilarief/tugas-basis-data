49e5b600d26fdf5781801da8cf17d8a155257d98
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-03-08 08:18:30 +0700

    add favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,6 +6,11 @@
 
 <title>Kelompok 10</title>
 
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
+<link rel="manifest" href="{{ asset('img/site.webmanifest') }}">
+
 <!-- Custom fonts for this template-->
 <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 <link
